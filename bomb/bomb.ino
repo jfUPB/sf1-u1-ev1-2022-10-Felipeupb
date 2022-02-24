06bdbd66cfe2bab23bb50e6d8a9bbed74c975ae0
felipeupb <felipe.aguilar@upb.edu.co>
2022-02-24 08:55:46 -0500

    add BombStates::INIT state

--- bomb/bomb.ino
+++ bomb/bomb.ino
@@ -6,14 +6,67 @@
 #define DOWN_BTN 32
 #define ARM_BTN 33
 
-// Selecciona uno según tu display.
-//SSD1306Wire display(0x3c, SDA, SCL, GEOMETRY_128_32);
-SSD1306Wire display(0x3c, SDA, SCL, GEOMETRY_64_48); 
+SSD1306Wire display(0x3c, SDA, SCL, GEOMETRY_64_48);
+
+void btnsTask();
+void bombTask();
 
 void setup() {
+  btnsTask();
+  bombTask();
 
 }
 
+// Eventos para comunicar
+bool evBtns = false;
+uint8_t evBtnsData = 0;
+
 void loop() {
+  btnsTask();
+  bombTask();
+}
+
+void btnsTask() {
+
+}
+
+void bombTask() {
+  enum class BombStates {INIT, DISARMED, ARMED};
+  static BombStates bombState =  BombStates::INIT;
+  static uint8_t counter;
+
+  switch (bombState) {
+    case BombStates::INIT: {
+        counter = 20;
+        display.init();
+        display.setContrast(255);
+        display.clear();
+        display.setTextAlignment(TEXT_ALIGN_LEFT);
+        display.setFont(ArialMT_Plain_16);
+
+        display.clear();
+        display.drawString(10, 20, String(counter));  
+        display.display();
+
+        bombState = BombStates::DISARMED;
+        break;
+      }
+    case BombStates::DISARMED: {
+
+        break;
+      }
+
+    case BombStates::ARMED: {
+
+        break;
+      }
+
+    default:
+      break;
+
+
+  }
+
+
 
 }
